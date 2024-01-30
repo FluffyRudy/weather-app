72e5fcf3be9724a5d656b23f0bcf1019e4241a44
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-01-30 19:03:39 +0545

    Initial Setup

---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+module.exports = {
+    entry: {
+        main: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            template: './src/template.html',
+        })
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(css)$/i,
+                use: ['style-loader', 'css-loader', "postcss-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        unused: true,
+                        dead_code: true,
+                    },
+                },
+            }),
+        ],
+    },
+};
+console.log(path.resolve(__dirname, 'src/assets'))
